334514cebe7e5d8cb4255164dab3a2caebbbc81e
eyesniper2 <eyesniper2@rayfall.net>
2016-05-21 14:04:33 -0600

    Added in Throttle Input/Motor Output

--- Esc-code.ino
+++ Esc-code.ino
@@ -1,30 +1,23 @@
 #include <Arduino.h>
 #include "hardware.h"
 #include "esc.h"
+#include <FLEXCAN.h>
+#include <kinetis_flexcan.h>
 
 volatile byte pulses;
 uint32_t total_pulses;
 float speed;
 
 uint8_t led_toggle = 0;
-void rpm_fun()
-{
- pulses++;
- total_pulses++;
- if(led_toggle == 0){
-  digitalWrite(led1,HIGH);
-  led_toggle = 1;
- }
- else
- {
-  digitalWrite(led1,LOW);
-  led_toggle = 0;
- }
-}
 
 uint32_t Serial_timer;
 unsigned long timeold;
 uint32_t Dyno_timer;
+
+int throttle_val = 10;
+int rawIn = 10;
+float minPedalVoltage = 140.0;
+int maxPedalVoltage = 1010;
 
 float WHEELCIRC;
 
@@ -32,33 +25,47 @@ int radius = 0.444;
 
 Esc myEsc;
 
-void setup()
-{
- pinMode(led1,OUTPUT);
- pinMode(led2,OUTPUT);
- pinMode(led3,OUTPUT);
+FlexCAN CANTx(500000);
+static CAN_message_t msg;
 
- Serial.begin(9600);
- delay(1000);
-
- //pins must be set to input before interrupt is attached
- pinMode(encoder_pin3, INPUT);
- pinMode(encoder_pin2, INPUT);
- pinMode(encoder_pin, INPUT);
- attachInterrupt(encoder_pin, rpm_fun, RISING);
- pulses = 0;
- WHEELCIRC = 2 * PI * radius;
-
- myEsc.begin();
- delay(15000);
-  timeold = millis();
-  Dyno_timer = millis();
- myEsc.write(170);
+void rpm_fun(){
+ pulses++;
+ total_pulses++;
+ if(led_toggle == 0){
+  digitalWrite(led1,HIGH);
+  led_toggle = 1;
+ }
+ else{
+  digitalWrite(led1,LOW);
+  led_toggle = 0;
+ }
 }
 
-void loop()
-{
-  myEsc.write(170);
+void setup(){
+	pinMode(led1,OUTPUT);
+	pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+
+	Serial.begin(9600);
+  delay(1000);
+
+  //pins must be set to input before interrupt is attached
+  pinMode(encoder_pin3, INPUT);
+  pinMode(encoder_pin2, INPUT);
+  pinMode(encoder_pin, INPUT);
+  attachInterrupt(encoder_pin, rpm_fun, RISING);
+  pulses = 0;
+  WHEELCIRC = 2 * PI * radius;
+
+  myEsc.begin();
+  delay(15000);
+	timeold = millis();
+  Dyno_timer = millis();
+
+	CANTx.begin();
+}
+
+void loop(){
  if (millis() - Serial_timer > 100)
  {
   Serial.flush();
@@ -82,15 +89,22 @@ void loop()
    timeold = millis();
    pulses = 0;
  }
- if(millis() - Dyno_timer > 50000)
- {
-  myEsc.write(0);
- }
- if(millis() - Dyno_timer > 80000)
- {
-  while(1)
-  {
-    myEsc.write(0);
-  }
- }
+
+ 	rawIn = analogRead(A11);
+	//int throttleVoltage = ((rawIn * 33)) / 10230;
+	throttle_val = ((rawIn - minPedalVoltage) / (maxPedalVoltage - minPedalVoltage)) * 180;
+	Serial.print("Raw In: ");
+	Serial.println(rawIn);
+	Serial.println("Throttle Value: ");
+	Serial.println(throttle_val);
+
+	if (throttle_val <= 180 && throttle_val >= 0){
+		myEsc.write(throttle_val);
+	}
+	else if (throttle_val > 180){
+		myEsc.write(180);
+	}
+	else{
+		myEsc.write(0);
+	}
 }
